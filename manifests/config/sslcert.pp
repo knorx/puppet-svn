34844326ee5ac00561460b3a2b0b51b0ffa63a3a
Ruben Stein <ruben.stein@mevis.fraunhofer.de>
2021-02-17 10:19:11 +0100

    FRIDAY-2304 only pass absent to file resource in case of absence, otherwise the template fails

--- manifests/config/sslcert.pp
+++ manifests/config/sslcert.pp
@@ -11,11 +11,20 @@ define svn::config::sslcert(
   if $ensure == present and (($realmstring == undef) or ($value == undef)) {
     fail('Realmstring and value are mandantory if ensure is present.')
   }
-  file { "${homedir}/auth/svn.ssl.server/${hash}":
-    ensure  => $ensure,
-    owner   => $owner,
-    group   => $group,
-    mode    => '0660',
-    content => template('svn/sslserver.erb')
+  $file_path = "${homedir}/auth/svn.ssl.server/${hash}"
+  if $ensure != absent {
+    file { $file_path:
+      ensure  => $ensure,
+      owner   => $owner,
+      group   => $group,
+      mode    => '0660',
+      content => template('svn/sslserver.erb')
+    }
   }
+  else {
+    file { $file_path:
+      ensure  => $ensure,
+    }
+  }
+
 }

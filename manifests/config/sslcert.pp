b00fe65afd0073359e58e284a52a5c6b2176ab2e
Ruben Stein <ruben.stein@mevis.fraunhofer.de>
2021-02-17 09:54:39 +0100

    FRIDAY-2304 allow ensure==absent for sslcert to delete old files. realmstring and value do not have to be defined for this

--- manifests/config/sslcert.pp
+++ manifests/config/sslcert.pp
@@ -1,13 +1,18 @@
 # SVN ssl certificate
 define svn::config::sslcert(
-  $realmstring,
-  $value,
+  $ensure = present,
+  Optional[String[1]] $realmstring = undef,
+  Optional[String[1]] $value = undef,
   $owner = $svn::config::owner,
   $group = $svn::config::group,
   $homedir = $svn::config::homedir,
   $hash = $name,
 ) {
+  if $ensure == present and (($realmstring == undef) or ($value == undef)) {
+    fail('Realmstring and value are mandantory if ensure is present.')
+  }
   file { "${homedir}/auth/svn.ssl.server/${hash}":
+    ensure  => $ensure,
     owner   => $owner,
     group   => $group,
     mode    => '0660',
